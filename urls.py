af47bf7354e9fdc71071f12c72edf23c09e7c73c
Jay Wineinger <jay.wineinger@gmail.com>
2013-04-23 22:55:39 -0500

    Revert "change auth url pattern"
    This reverts commit 16d6c88f782e4d9d683780a93dc192310292f52f.

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 import views
 
 urlpatterns = patterns('',
+	url(r'^auth(?P<mount>.*)$', views.redirect_with_token, name='stream_auth'),
     url(r'^listener/add/$', views.listener_add, name='listener_add'),
     url(r'^listener/remove/$', views.listener_remove, name='listener_remove'),
-	url(r'^(?P<mount>.*)$', views.redirect_with_token, name='stream_auth'),
 )
